6483c142bcf9709427da9b31482f09e07eae5f5f
trinity <108339048+ummhii@users.noreply.github.com>
2024-10-11 14:21:22 +1100

    fixed error with cursor & added convert type choices

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,33 @@ use std::io::{Cursor};
 use std::path::{Path};
 use image::{ImageReader};
 
-fn convert_file(path: &Path) {
-    // TODO
-    let img = ImageReader::open(path).unwrap().decode().unwrap();
+fn filestr_to_enum(file_type: &str) -> image::ImageFormat {
+    match file_type {
+        "jpg" => image::ImageFormat::Jpeg,
+        "jpeg" => image::ImageFormat::Jpeg,
+        "png" => image::ImageFormat::Png,
+        _ => {image::ImageFormat::Jpeg}
+    }
+}
 
+fn convert_file(path: &Path, file_type: &String) {
+    let img = ImageReader::open(path).unwrap().decode().unwrap();
+    let format = filestr_to_enum(file_type.as_str());
     let file_name = path.file_name().unwrap().to_str().unwrap();
-    let new_file_name = file_name.replace(".webp", ".jpg");
+    let new_file_extension = ".".to_owned() + &*file_type.to_string();
+    let new_file_name = file_name.replace(".webp", &*new_file_extension);
     img.save(&new_file_name).expect("saving file failed");
 
     let mut bytes: Vec<u8> = Vec::new();
-    img.write_to(&mut Cursor::new(&mut bytes), image::ImageFormat::Jpeg).unwrap();
+    img.write_to(&mut Cursor::new(&mut bytes), format).unwrap();
 
-    let img2 = ImageReader::new(Cursor::new(bytes.as_ref())).with_guessed_format().unwrap().decode().unwrap();
-    img2.write_to(&mut Cursor::new(&mut bytes), image::ImageFormat::Jpeg).unwrap();
+    let img2 = ImageReader::new(Cursor::<&mut Vec<u8>>::new(bytes.as_mut())).with_guessed_format().unwrap().decode().unwrap();
+    img2.write_to(&mut Cursor::new(&mut bytes), format).unwrap();
 
     println!("image converted");
 }
 
-fn traverse_files(path: &Path, total_files: &mut u32) {
+fn traverse_files(path: &Path, total_files: &mut u32, file_type: &String) {
     let files = fs::read_dir(path).unwrap();
     if path.is_dir() {
         println!("Scanning directory {}", path.display().to_string());
@@ -28,10 +37,10 @@ fn traverse_files(path: &Path, total_files: &mut u32) {
             let file = file.unwrap();
             let path = file.path();
             if path.is_dir() {
-                traverse_files(&path, total_files);
-            } else if path.extension().unwrap() == "webp" {
+                traverse_files(&path, total_files, file_type);
+            } else if path.extension().unwrap().to_str().unwrap() == file_type.to_string() {
                 *total_files += 1;
-                convert_file(&path);
+                convert_file(&path, &file_type);
             }
         }
     }
@@ -39,8 +48,9 @@ fn traverse_files(path: &Path, total_files: &mut u32) {
 
 fn main() {
     let flag: String = env::args().nth(1).unwrap();
+    let file_type: String = env::args().nth(2).unwrap();
     let path: &Path = Path::new(&flag);
     let mut total_webp_files = 0;
-    traverse_files(&path, &mut total_webp_files);
+    traverse_files(&path, &mut total_webp_files, &file_type);
     println!("\n{} webp files in {} converted", total_webp_files, path.display().to_string());
 }
