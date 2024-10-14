fd3f402b39dc537ce8d817e4a559fdc846e9df53
trinity <108339048+ummhii@users.noreply.github.com>
2024-10-15 01:05:40 +1100

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,28 @@
 use std::{env, fs};
-use std::io::{Cursor};
-use std::path::{Path};
-use image::{ImageReader};
+use std::io::Cursor;
+use std::path::Path;
+use image::ImageReader;
 
-fn filestr_to_enum(file_type: &str) -> image::ImageFormat {
-    match file_type {
+fn filestr_to_enum(file_type: String) -> image::ImageFormat {
+    match file_type.as_str() {
         "jpg" => image::ImageFormat::Jpeg,
         "jpeg" => image::ImageFormat::Jpeg,
         "png" => image::ImageFormat::Png,
+        "gif" => image::ImageFormat::Gif,
+        "bmp" => image::ImageFormat::Bmp,
+        "ico" => image::ImageFormat::Ico,
+        "webp" => image::ImageFormat::WebP,
         _ => {image::ImageFormat::Jpeg}
     }
 }
 
-fn convert_file(path: &Path, file_type: &String) {
+fn convert_file(path: &Path, file_type: &mut String) {
     let img = ImageReader::open(path).unwrap().decode().unwrap();
-    let format = filestr_to_enum(file_type.as_str());
-    let file_name = path.file_name().unwrap().to_str().unwrap();
+    let format = filestr_to_enum((*file_type.as_mut()).parse().unwrap());
     let new_file_extension = ".".to_owned() + &*file_type.to_string();
-    let new_file_name = file_name.replace(".webp", &*new_file_extension);
-    img.save(&new_file_name).expect("saving file failed");
+    let new_file_path = path.to_str().unwrap().replace(".webp", &*new_file_extension);
+    img.save(&new_file_path).expect("saving file failed");
+    fs::remove_file(path).expect("removing file failed");
 
     let mut bytes: Vec<u8> = Vec::new();
     img.write_to(&mut Cursor::new(&mut bytes), format).unwrap();
@@ -29,7 +33,7 @@ fn convert_file(path: &Path, file_type: &String) {
     println!("image converted");
 }
 
-fn traverse_files(path: &Path, total_files: &mut u32, file_type: &String) {
+fn traverse_files(path: &Path, total_files: &mut u32, file_type: &mut String) {
     let files = fs::read_dir(path).unwrap();
     if path.is_dir() {
         println!("Scanning directory {}", path.display().to_string());
@@ -38,19 +42,19 @@ fn traverse_files(path: &Path, total_files: &mut u32, file_type: &String) {
             let path = file.path();
             if path.is_dir() {
                 traverse_files(&path, total_files, file_type);
-            } else if path.extension().unwrap().to_str().unwrap() == file_type.to_string() {
+            } else if path.extension().unwrap().to_str().unwrap() == "webp" {
                 *total_files += 1;
-                convert_file(&path, &file_type);
+                convert_file(&path, file_type);
             }
         }
     }
 }
 
 fn main() {
-    let flag: String = env::args().nth(1).unwrap();
-    let file_type: String = env::args().nth(2).unwrap();
-    let path: &Path = Path::new(&flag);
+    let path_arg: String = env::args().nth(1).expect("no path given");
+    let mut file_type_arg: String = env::args().nth(2).expect("no file type given");
+    let path: &Path = Path::new(&path_arg);
     let mut total_webp_files = 0;
-    traverse_files(&path, &mut total_webp_files, &file_type);
+    traverse_files(&path, &mut total_webp_files, &mut file_type_arg);
     println!("\n{} webp files in {} converted", total_webp_files, path.display().to_string());
 }
